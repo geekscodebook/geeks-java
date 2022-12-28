1ba1d485d3a600f1fa9cf9c983971408c60ec11b
Sunil Yadav <sunil@Sunils-MacBook-Pro.local>
2022-12-27 22:45:27 -0500

    Initial commit

---
+++ elastic/src/main/java/com/geeksjava/elastic/App.java
@@ -0,0 +1,13 @@
+package com.geeksjava.elastic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
